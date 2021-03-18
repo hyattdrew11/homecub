d9fa663b8ca89ccc057f3627ce34237731e61f25
Drew Hyatt <drew@cicayda.com>
2021-03-18 17:37:05 -0500

    Initial Commit

---
+++ Homecub/HomecubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomecubApp.swift
+//  Homecub
+//
+//  Created by Drew Hyatt on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct HomecubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
